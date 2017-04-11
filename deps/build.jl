686ab641f4204b15a92139b34a16fcaf6ef7d2fa
Mus M <mus-m@outlook.com>
2017-04-11 16:39:55 -0400

    Fix build for windows (#31)
    * Fix build for windows

--- deps/build.jl
+++ deps/build.jl
@@ -1,19 +1,20 @@
 using BinDeps
+using Compat
+
 @BinDeps.setup
 libnames = ["libCLBLAS", "clBLAS", "libclBLAS"]
 libCLBLAS = library_dependency("libCLBLAS", aliases = libnames)
-archive = "package"
-libpath = "package/bin"
 baseurl = "https://github.com/clMathLibraries/clBLAS/releases/download/v2.12/clBLAS-2.12.0-"
 
 # download a pre-compiled binary (built by GLFW)
 if is_windows()
     if Sys.ARCH == :x86_64
         uri = URI(baseurl * "Windows-x64.zip")
+        basedir = joinpath(@__DIR__, "package")
         provides(
             Binaries, uri,
-            libCLBLAS, unpacked_dir = archive,
-            installed_libpath = "usr/lib64", os = :Windows
+            libCLBLAS, unpacked_dir = basedir,
+            installed_libpath = joinpath(basedir, "bin"), os = :Windows
         )
     else
         error("Only 64 bits windows supported with automatic build")
@@ -21,11 +22,10 @@ if is_windows()
 end
 
 if is_linux()
-
     if Sys.ARCH == :x86_64
         push!(BinDeps.defaults, Binaries)
         uri = URI(baseurl * "Linux-x64.tar.gz")
-        basedir = joinpath(dirname(@__FILE__), "clBLAS-2.12.0-Linux-x64")
+        basedir = joinpath(@__DIR__, "package")
         provides(
             Binaries, uri,
             libCLBLAS, unpacked_dir = basedir,
@@ -34,6 +34,7 @@ if is_linux()
     end
     provides(AptGet, "libclblas-dev", libCLBLAS)
 end
+
 if is_apple()
     error("""
         OSX not oficially supported.
