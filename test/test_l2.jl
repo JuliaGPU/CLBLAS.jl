422ce95396c26357081cb8dd9a36714446986208
Sheehan Olver <solver@mac.com>
2018-06-01 10:32:41 +0100

    add gbmv

--- test/test_l2.jl
+++ test/test_l2.jl
@@ -15,4 +15,22 @@
             @test cl.to_host(C) ≈ hC
         end
     end
+
+    @testset "CLBLAS gbmv" begin
+        for T in testtypes
+            alpha = T(2.0)
+            beta = T(0.0)
+            hA = rand(T, 4, 32)
+            hB = rand(T, 32)
+            m = 33
+            hC = rand(T, m)
+            A = CLArray(q, hA)
+            B = CLArray(q, hB)
+            C = CLArray(q, hC)
+            cl.wait(gbmv!('N', m, 1, 2, alpha, A, B, beta, C))
+            gbmv!('N', m, 1, 2, alpha, hA, hB, beta, hC)
+
+            @test cl.to_host(C) ≈ hC
+        end
+    end
 end
